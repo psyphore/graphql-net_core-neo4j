bde77dd6f01a33a63a5d0a0a4d4b99d20be7230e
Sipho Hlophe <psyphore@gmail.com>
2023-03-08 11:30:12 +0200

    feat: add graphql client

---
+++ src/ThumbezaTech.Leads.ClientLibrary/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ThumbezaTech.Leads.ClientLibrary;
+
+public static class DependencyInjection
+{
+  public static IServiceCollection AddClient(this IServiceCollection services, IConfigurationSection section)
+  {
+    ClientConfiguration config = new();
+    section.Bind(config);
+
+    services.AddSingleton(config);
+
+    //services
+    //  .AddLeadsClient()
+    //  .ConfigureHttpClient(client =>
+    //  {
+    //    client.BaseAddress = new Uri(config.Url);
+    //  });
+
+    return services;
+  }
+}
+
+public sealed record ClientConfiguration
+{
+  public string Url { get; set; }
+  public string Key { get; set; }
+}
