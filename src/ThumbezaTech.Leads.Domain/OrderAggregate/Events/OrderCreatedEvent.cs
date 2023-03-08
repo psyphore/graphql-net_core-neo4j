fc4a4ccbc4deacfad641d9fb4a27e84e676cdf6e
Sipho Hlophe <psyphore@gmail.com>
2023-03-08 10:44:46 +0200

    feat: add order aggregate

---
+++ src/ThumbezaTech.Leads.Domain/OrderAggregate/Events/OrderCreatedEvent.cs
@@ -0,0 +1,21 @@
+﻿namespace ThumbezaTech.Leads.Domain.OrderAggregate.Events;
+public sealed class OrderCreatedEvent : BaseDomainEvent
+{
+  public Order Order { get; private set; }
+  public OrderCreatedEvent(Order order) => Order = order;
+}
+
+
+public sealed class OrderUpdatedEvent : BaseDomainEvent
+{
+  public Order Order { get; private set; }
+  public OrderUpdatedEvent(Order order) => Order = order;
+}
+
+
+public sealed class OrderConfirmedEvent : BaseDomainEvent
+{
+  public Order Order { get; private set; }
+  public OrderConfirmedEvent(Order order) => Order = order;
+}
+
---
+++ src/ThumbezaTech.Leads.Domain/OrderAggregate/Order.cs
@@ -0,0 +1,7 @@
+﻿using ThumbezaTech.Leads.SharedKernel.Interfaces;
+
+namespace ThumbezaTech.Leads.Domain.OrderAggregate;
+public sealed class Order : BaseEntity, IAggregateRoot
+{
+
+}
