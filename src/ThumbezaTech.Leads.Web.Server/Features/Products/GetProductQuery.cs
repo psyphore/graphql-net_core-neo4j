f6775859f42ebf5041891b44e9526ef16fd1f2a7
Sipho Hlophe <psyphore@gmail.com>
2023-03-11 09:53:37 +0200

    feat: start consuming the generated graphql client

--- src/ThumbezaTech.Leads.Web.Server/Features/Products/GetProductQuery.cs
+++ src/ThumbezaTech.Leads.Web.Server/Features/Products/GetProductQuery.cs
@@ -1,4 +1,8 @@
-﻿namespace ThumbezaTech.Leads.Web.Server.Features.Products;
+﻿using ThumbezaTech.Leads.ClientLibrary;
+using ThumbezaTech.Leads.ClientLibrary.State;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ThumbezaTech.Leads.Web.Server.Features.Products;
 
 public record GetProductQuery(int Page, int Size) : IRequest;
 
@@ -6,20 +10,29 @@ internal sealed class GetProductQueryHandler : IRequestHandler<GetProductQuery>
 {
   private readonly ProductState _state;
   private readonly ILogger<GetProductQueryHandler> _logger;
+  private readonly IGet_productsQuery _query;
 
-  public GetProductQueryHandler(ProductState state, ILogger<GetProductQueryHandler> logger)
+  public GetProductQueryHandler(ProductState state, ILogger<GetProductQueryHandler> logger, IGet_productsQuery query)
   {
     _state = state;
     _logger = logger;
+    _query = query;
   }
 
   public async Task Handle(GetProductQuery request, CancellationToken cancellationToken)
   {
     try
     {
-      await Task.Delay(500, cancellationToken);
-      var content = Enumerable.Range(1, request.Size).Select(i => new ProductVm($"{i}", $"Product {i}", "123456789123", 15.999M));
-      _state.SetStateItems(content.ToArray());
+      var content = await _query.ExecuteAsync(cancellationToken);
+      if(content.Errors.Any())
+      {
+        _logger.LogWarning("something didn't go well while fetching products {@Errors}", content.Errors);
+      }
+
+      var data = content.Data.List_products.Nodes;
+      //await Task.Delay(500, cancellationToken);
+      //var content = Enumerable.Range(1, request.Size).Select(i => new ProductVm($"{i}", $"Product {i}", "123456789123", 15.999M));
+      //_state.SetStateItems(content.ToArray());
     }
     catch (Exception e)
     {
