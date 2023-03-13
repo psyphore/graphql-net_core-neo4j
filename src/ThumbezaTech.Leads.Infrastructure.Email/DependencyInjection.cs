45a56b033895dd3b346d5b56da3e8b5d3d400dc7
Sipho Hlophe <psyphore@gmail.com>
2023-03-13 07:52:49 +0200

    feat: add email capability

---
+++ src/ThumbezaTech.Leads.Infrastructure.Email/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using FluentEmail.MailKitSmtp;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using ThumbezaTech.Leads.Application.Common.Email;
+using ThumbezaTech.Leads.Infrastructure.Email.Services;
+
+namespace ThumbezaTech.Leads.Infrastructure.Email;
+public static class DependencyInjection
+{
+  public static IServiceCollection AddEmailInfrastructure(this IServiceCollection services, IConfigurationSection section)
+  {
+    SmtpConfiguration conf = new();
+    section.Bind(conf);
+
+    services.AddSingleton(sp => conf);
+
+    services
+            .AddFluentEmail(conf.Sender, conf.SenderName)
+            .AddRazorRenderer()
+            .AddMailKitSender(new SmtpClientOptions
+            {
+              Server = conf.SmtpServer,
+              Port = conf.Port
+            });
+
+    services.AddHttpContextAccessor();
+    services.AddRazorPages();
+
+    services.AddTransient<ISendEmailService, SendEmailService>();
+
+    return services;
+  }
+}
+
+internal sealed record SmtpConfiguration
+{
+  public string Sender { get; set; }
+  public string SenderName { get; set; }
+  public string SmtpServer { get; set; }
+  public int Port { get; set; }
+}
---
+++ src/ThumbezaTech.Leads.Infrastructure.Email/Services/SendEmailService.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.Result;
+
+using FluentEmail.Core;
+
+using ThumbezaTech.Leads.Application.Common.Email;
+
+namespace ThumbezaTech.Leads.Infrastructure.Email.Services;
+internal sealed class SendEmailService : ISendEmailService
+{
+  private readonly IFluentEmailFactory _factory;
+  public SendEmailService(IFluentEmailFactory factory) => _factory = factory;
+
+  public async ValueTask<Result> SendEmailAsync(Dictionary<string, object> message, CancellationToken cancellationToken)
+  {
+    var email = _factory
+            .Create()
+            .To(Translate<string>("Receipent", message))
+            .Subject(Translate<string>("Subject", message))
+            .Body(Translate<string>("Message", message), true);
+
+    var response = await email.SendAsync(cancellationToken);
+    return response.Successful
+      ? Result.SuccessWithMessage(response.MessageId)
+      : Result.Error(string.Join("; ", response.ErrorMessages));
+  }
+
+  private static T Translate<T>(string key, IDictionary<string, object> payload)
+    => (T)payload[key];
+}
