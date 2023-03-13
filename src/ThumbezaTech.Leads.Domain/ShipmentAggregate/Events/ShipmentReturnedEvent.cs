4c6206823714c73c2915ff8c48071a26bcc688a4
Sipho Hlophe <psyphore@gmail.com>
2023-03-13 07:59:41 +0200

    feat: add shipment aggregate and its events

---
+++ src/ThumbezaTech.Leads.Domain/ShipmentAggregate/Events/ShipmentReturnedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace ThumbezaTech.Leads.Domain.ShipmentAggregate.Events;
+
+public sealed class ShipmentReturnedEvent : BaseDomainEvent
+{
+  public ShipmentReturnedEvent(Shipment shipment) => Shipment = shipment;
+  public Shipment Shipment { get; }
+}
+
+
---
+++ src/ThumbezaTech.Leads.Domain/ShipmentAggregate/Events/ShipmentUpdatedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace ThumbezaTech.Leads.Domain.ShipmentAggregate.Events;
+
+public sealed class ShipmentUpdatedEvent : BaseDomainEvent
+{
+  public ShipmentUpdatedEvent(Shipment shipment) => Shipment = shipment;
+  public Shipment Shipment { get; }
+}
+
+
