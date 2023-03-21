fd45b3c8e8deb0c6c18fbc86ba84011b7d2f027f
Sipho Hlophe <psyphore@gmail.com>
2023-03-21 11:04:23 +0200

    test: run tests on ci cd pipeline

--- tests/ThumbezaTech.Leads.UnitTests/Application/Leads/UpdateLeadHandlerHandle.cs
+++ tests/ThumbezaTech.Leads.UnitTests/Application/Leads/UpdateLeadHandlerHandle.cs
@@ -82,4 +82,41 @@ public class UpdateLeadHandlerHandle
     _sender.Verify(s => s.Send(new GetLeadByIdQuery(lead.Id), CancellationToken.None), Times.Once);
     _service.Verify(s => s.UpdateLeadAsync(lead, CancellationToken.None), Times.Once);
   }
+
+  [Fact]
+  public async Task UpdateLeadMissingInstance()
+  {
+    var lead = new Lead(
+      _faker.Person.FirstName,
+      _faker.Person.LastName,
+      _faker.Person.DateOfBirth,
+      false,
+      new[]
+      {
+        new Contact(_faker.Person.Phone, _faker.Person.Email)
+      },
+      new[]
+      {
+        new Address(_faker.Address.BuildingNumber(),
+                    _faker.Address.StreetAddress(),
+                    default!,
+                    _faker.Address.StreetSuffix(),
+                    _faker.Address.ZipCode(),
+                    _faker.Address.Country())
+      })
+    { Id = _faker.Random.Uuid().ToString() };
+
+    _sender
+      .Setup(s => s.Send(It.IsAny<GetLeadByIdQuery>(), CancellationToken.None))
+      .ReturnsAsync(Result.NotFound());
+
+    _service
+      .Setup(s => s.UpdateLeadAsync(It.IsAny<Lead>(), CancellationToken.None))
+      .ReturnsAsync(Result.SuccessWithMessage(lead.Id));
+
+    Func<Task> act = async () => await _handler.Handle(new UpdateLeadCommand(lead), CancellationToken.None);
+    await act.Should().NotThrowAsync<ArgumentNullException>();
+    _sender.Verify(s => s.Send(new GetLeadByIdQuery(lead.Id), CancellationToken.None), Times.Once);
+    _service.Verify(s => s.UpdateLeadAsync(lead, CancellationToken.None), Times.Never);
+  }
 }
